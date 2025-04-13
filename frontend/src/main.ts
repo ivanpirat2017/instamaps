3a78748b9e6e00468c3e8e5865891fe6e075c0dc
ivancgi <ivan62196211@gmail.com>
2025-04-13 13:44:53 +0300

    init frontend

---
+++ frontend/src/main.ts
@@ -0,0 +1,19 @@
+import { createVuestic } from 'vuestic-ui'
+import 'vuestic-ui/styles/essential.css'
+import 'vuestic-ui/styles/typography.css'
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+
+app.use(createVuestic());
+app.mount('#app')
